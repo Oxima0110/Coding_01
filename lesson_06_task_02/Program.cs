70ca909b2ba0332c52dbaf353c8a994853232c24
Oxana <crazyglass@list.ru>
2022-08-05 23:02:45 +0300

    Быстрая сортировка по первому опорному элементу

---
+++ lesson_06_task_02/Program.cs
@@ -0,0 +1,39 @@
+﻿
+int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
+// int[] res = QuickSort(arr, 0, arr.Length - 1);
+QuickSort(arr, 0, arr.Length -1);
+Console.Write($"Отсортированный массив: [{string.Join(", ", arr)}]");
+
+void QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex) return;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return;
+}
+
+int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = maxIndex + 1;
+    for (int i = maxIndex; i > minIndex; i--)
+    {
+        if (inputArray[i] > inputArray[minIndex])
+        {
+            pivotIndex--;
+            Swap(inputArray, i, pivotIndex);
+        }
+    }
+    pivotIndex--;
+    Swap(inputArray, minIndex, pivotIndex);
+    return pivotIndex;
+}
+
+void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[rightValue];
+    inputArray[rightValue] = inputArray[leftValue];
+    inputArray[leftValue] = temp;
+}
+
+
