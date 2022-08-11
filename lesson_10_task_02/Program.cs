14d2284361be70a0fb7b2f55630ccb374d9ec6d8
Oxana <crazyglass@list.ru>
2022-08-11 13:01:26 +0300

    Сортировка подсчетом числа

---
+++ lesson_10_task_02/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.Clear();
+
+int[] array = { 0, 2, 4, 10, 20, 5, 6, 1, 2 };
+int[] sortedArray =  CountingSortExtended(array);
+
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", sortedArray));
+
+
+int[] CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int[] sortedArray = new int[inputArray.Length];
+    int[] counters = new int[max + 1];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i;
+            index++;
+        }
+    }
+
+    return sortedArray;
+}
